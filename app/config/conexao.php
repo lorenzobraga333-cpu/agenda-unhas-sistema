19fc753ebb0f7436a057dbdac9813e86bbda05e1
Lorenzo Castagnetti <lorenzobraga333@gmail.com>
2026-06-05 20:31:12 -0300

    adiciona dotenv, composer e configurações de ambiente

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -1,9 +1,13 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "agenda_unhas";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$database = $_ENV['DB_NAME'];
 
 $conn = new mysqli($host, $user, $password, $database);
 
